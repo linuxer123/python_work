5ad08a37f75925e3b6135cc6d2ca0ebeb5af0c43
john <qq@qq.com>
2019-08-01 16:22:53 +0800

    first commit

---
+++ class.py
@@ -0,0 +1,16 @@
+class Dog():
+
+	def __init__(self,name,type):
+		self.name=name
+		self.type=type
+	
+	def getName(self,name):
+		print(self.name)
+		
+
+class eDog(Dog):
+	def __init__(self,name,type):
+		self.name=named
+		self.type=type
+	
+	
